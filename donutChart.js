11b3cd4c1c8658057411d57a3b5fa4f3c16b6512
TusharDhanai <dhanai.tushar007@gmail.com>
2022-10-16 20:50:06 +0530

    Make donut chart dynamic.

--- donutChart.js
+++ donutChart.js
@@ -1,7 +1,10 @@
-$(function () {
-    var data = [44, 55, 41, 17]
-    var labels = ['Apple', 'Mango', 'Orange', 'Watermelon']
+const initialData = [44, 55, 41, 17]
+const initialLabels = ['Apple', 'Mango', 'Orange', 'Watermelon']
 
+var data = [44, 55, 41, 17]
+var labels = ['Apple', 'Mango', 'Orange', 'Watermelon']
+
+$(function () {
     var options = {
         series: data,
         labels: labels,
@@ -35,4 +38,65 @@ $(function () {
 
     var chart = new ApexCharts(document.querySelector("#donut-chart"), options);
     chart.render();
-})
+
+    $('#donut-add').on('click',function(e) {
+        e.preventDefault();
+
+        let label = $('#donut-name').val();
+        let value = $('#donut-value').val();
+
+        if (label === '' || value === '') {
+            console.error('Empty values.')
+            return
+        }
+
+        labels.push(label);
+        data.push(Number(value));
+
+        chart.updateOptions({
+            series: data,
+            labels: labels
+        })
+
+        $('#donut-name').val('');
+        $('#donut-value').val('');
+
+        $('#donut-select').append(`
+            <option value="${value}">${label}</option>
+        `);
+    });
+
+    $('#donut-reset').on('click',function(e) {
+        e.preventDefault();
+
+        labels = [...initialLabels]
+        data = [...initialData]
+
+        chart.updateOptions({
+            series: data,
+            labels: labels
+        })
+    })
+
+    $('#donut-select').on('change',function() {
+        let label = this.selectedOptions[0].innerText
+
+        let index = labels.indexOf(label)
+
+        if (index === -1)
+        {
+            console.error('Index not found.');
+            return;
+        }
+
+        labels.splice(index, 1)
+        data.splice(index, 1)
+
+        this.selectedOptions[0].remove()
+
+        chart.updateOptions({
+            series: data,
+            labels: labels
+        })
+    })
+});
